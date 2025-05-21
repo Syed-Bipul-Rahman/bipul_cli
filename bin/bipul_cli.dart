5057059526e8453e3815ad8996b2d9d7a02d1b74
Bipul <123935352+Syed-Bipul-Rahman@users.noreply.github.com>
2025-05-21 20:27:21 +0600

    dksafj

---
+++ bin/bipul_cli.dart
@@ -0,0 +1,5 @@
+import 'package:bipul_cli/bipul_cli.dart' as bipul_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${bipul_cli.calculate()}!');
+}
---
+++ test/bipul_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:bipul_cli/bipul_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
